27b77f89a21a898fcfeb69a680ba6eee580ab058
hjlim <teddycns3@gmail.com>
2022-06-29 15:21:27 +0900

    React Hook 적용(22/06/29)

---
+++ react-basic/habit-tracker/src/index.js
@@ -0,0 +1,23 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js';
+import SimpleHabit from './components/simpleHabit';
+
+// react v17까지는 아래와 같이 코드 작성
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root') // root라는 id에 App component를 연결한다. (index.html에 root id 선언)
+// );
+
+
+// react v18에서는 이렇게 코드 작성
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode >
+        <SimpleHabit />
+    </StrictMode>,
+);
