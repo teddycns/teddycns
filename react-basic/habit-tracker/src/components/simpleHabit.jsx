fc94f233e49fd6456fb31af1aece452c8499f2f8
hjlim <teddycns3@gmail.com>
2022-06-29 13:50:58 +0900

    reack Hook (simpleHabit.jsx로 간단하게 구현) _ 22/06/29

---
+++ react-basic/habit-tracker/src/components/simpleHabit.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const SimpleHabit = props => {
+    const [count, setCount] = useState(0); // useState(초기값)
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <li className='habit'>
+            <span className="habit-name">Reading</span>
+            <span className="habit-count">{count}</span>
+            <button className="habit-button habit-increase" onClick={handleIncrement}>
+                <i className="fa-solid fa-square-plus"></i>
+            </button>
+        </li>
+    );
+};
+
+export default SimpleHabit;
