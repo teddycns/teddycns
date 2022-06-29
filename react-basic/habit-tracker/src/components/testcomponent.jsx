3ac360eaa8b4bcf0a62bbca7c3194dd8ba71ddff
hjlim <teddycns3@gmail.com>
2022-06-29 15:51:30 +0900

    commit test

---
+++ react-basic/habit-tracker/src/components/testcomponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class testcomponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default testcomponent;
